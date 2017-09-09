733dd9748ed0d54cf57be10063e745e0b97da7f0
ashanaakh <ashanaakh@gmail.com>
2017-09-10 00:07:59 +0300

    cleanup & cli

--- src/solver.h
+++ src/solver.h
@@ -2,28 +2,22 @@
 #define solver_h
 
 #include <iostream>
-#include <chrono>
+// #include <chrono>
 #include <thread>
 #include <condition_variable>
 #include <mutex>
 
-#include <string>
-
 using namespace std;
+using namespace literals;
 
 class Solver {
 
   using func = function<void()>;
-
-  bool res1, res2;
-
-  bool done;
-
+  bool res1, res2, done;
   condition_variable cv;
-
   mutex m;
 
-  void testfunc1(func f);
+  void testFunc(func f);
 
 public:
 
